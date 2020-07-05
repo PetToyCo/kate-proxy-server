c6cf4ce85a2cbcb896ffe0b17a209aa3c861b5ba
KatrinKatrin <kate.uraltceva@gmail.com>
2020-07-05 13:17:06 -0700

    kate proxy server

--- server/index.js
+++ server/index.js
@@ -1,19 +1,11 @@
 const express = require('express');
-const httpProxy = require('http-proxy');
-const apiProxy = httpProxy.createProxyServer();
-const photoGallery = 'http://localhost:3003';
-
+const path = require('path');
 const app = express();
-const PORT = 3004;
-
-app.all("/itemImages/*", function(req, res) {
-    console.log('redirecting to Photo Gallery');
-    apiProxy.web(req, res, {target: photoGallery});
-});
-
 
 app.use(express.static(__dirname + '/../client'));
 
-app.listen(PORT, () => {
-  console.log(`listening on port ${PORT}`);
-});
+app.get('/product', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/index.html'));
+})
+
+module.exports = app;
